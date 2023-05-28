24220b0634651710b499ff4b96f0af42362b5cc4
Madan gopal <madangopalsharma2019@gmail.com>
2023-05-28 17:28:03 +0530

    added message in sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class sum{
     public static void main(String args[]){
         int a=5;
         int b=6;
-        System.out.println(a+b);
+        System.out.println("sum of a and b"+a+b);
     }
 }
